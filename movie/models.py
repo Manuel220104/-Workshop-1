e8d1331f6423039c888b29a51465eef4a6e35678
manuel220104 <marangog3eafit.edu.co>
2023-08-24 11:31:02 -0500

    taller 2

--- movie/models.py
+++ movie/models.py
@@ -5,5 +5,7 @@ from django.db import models
 class Movie(models.Model):
     title = models.CharField(max_length = 100)
     description = models.CharField(max_length = 250)
-    image = models.ImageField(upload_to = 'movie/images/')
+    image = models.ImageField(upload_to = 'movie/media/')
     url = models.URLField(blank = True)
+
+
--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'movie',
+    'news',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'moviereviews.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'moviereviews/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    'moviereviews/static/',
+]
